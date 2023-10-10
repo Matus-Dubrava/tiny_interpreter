33303002aed73e0f3ba1cd01cf23cdabd97ec1d1
Matus Dubrava <matusdubrava@gmail.com>
2023-10-10 13:28:19 +0200

    infix opeator eval

--- evaluator/__tests__/evaluator.ts
+++ evaluator/__tests__/evaluator.ts
@@ -27,12 +27,45 @@ test('test evaluate integer expression', () => {
         { input: '0', expected: 0 },
         { input: '-1', expected: -1 },
         { input: '-0', expected: 0 },
+        { input: '5 + 5 + 5 + 5 - 10', expected: 10 },
+        { input: '2 * 2 * 2 * 2 * 2', expected: 32 },
+        { input: '-50 + 100 + -50', expected: 0 },
+        { input: '5 * 2 + 10', expected: 20 },
+        { input: '5 + 2 * 10', expected: 25 },
+        { input: '20 + 2 * -10', expected: 0 },
+        { input: '50 / 2 * 2 + 10', expected: 60 },
+        { input: '2 * (5 + 10)', expected: 30 },
+        { input: '3 * 3 * 3 + 10', expected: 37 },
+        { input: '3 * (3 * 3) + 10', expected: 37 },
+        { input: '(5 + 10 * 2 + 15 / 3) * 2 + -10', expected: 50 },
+        { input: '10 % 2', expected: 0 },
+        { input: '10 % 3', expected: 1 },
+        { input: '0 % 2', expected: 0 },
     ];
 
     tests.forEach(({ input, expected }) => {
         const evaluated = testEval(input);
         expect(evaluated).not.toBeNull();
         testIntegerObject(evaluated!, expected);
+    });
+});
+
+test('test evaluate infix boolean expression', () => {
+    const tests = [
+        { input: 'true && true', expected: true },
+        { input: 'true && false', expected: false },
+        { input: 'true && false', expected: false },
+        { input: 'false && false', expected: false },
+        { input: 'true || true', expected: true },
+        { input: 'true || false', expected: true },
+        { input: 'true || false', expected: true },
+        { input: 'false || false', expected: false },
+    ];
+
+    tests.forEach(({ input, expected }) => {
+        const evaluated = testEval(input);
+        expect(evaluated).not.toBeNull();
+        testBooleanObject(evaluated!, expected);
     });
 });
 
--- evaluator/index.ts
+++ evaluator/index.ts
@@ -8,6 +8,7 @@ import {
     BooleanLiteral,
     PrefixExpression,
     IExpression,
+    InfixExpression,
 } from '../ast';
 
 const TRUE = new BooleanObj(true);
@@ -26,20 +27,125 @@ export function evaluate(node: INode): IObject | null {
     } else if (node instanceof PrefixExpression) {
         const right = evaluate(node.expr);
         if (right) {
-            return evaluatePrefixExpression(right, node.operator);
+            return evaluatePrefixExpression(node.operator, right);
         }
+        // do we want to return null or error here
+        return null;
+    } else if (node instanceof InfixExpression) {
+        const left = evaluate(node.left);
+        const right = evaluate(node.right);
+        if (left && right) {
+            return evaluateInfixExpression(left, node.operator, right);
+        }
+        // do we want to return null or error here
+        return null;
     }
 
     return null;
 }
 
-function evaluatePrefixExpression(right: IObject, operator: string): IObject {
+function evaluateInfixExpression(
+    left: IObject,
+    operator: string,
+    right: IObject
+): IObject {
+    switch (operator) {
+        case '+':
+            return evaluateInfixPlus(left, right);
+        case '-':
+            return evaluateInfixMinus(left, right);
+        case '*':
+            return evaluateInfixAsterisk(left, right);
+        case '/':
+            return evaluateInfixSlash(left, right);
+        case '%':
+            return evaluateInfixReminder(left, right);
+        case '||':
+            return evaluateInfixOr(left, right);
+        case '&&':
+            return evaluateInfixAnd(left, right);
+        default:
+            return new ErrorObj(`unknown infix operator '${operator}'`);
+    }
+}
+
+function evaluateInfixOr(left: IObject, right: IObject): IObject {
+    if (left instanceof BooleanObj && right instanceof BooleanObj) {
+        return new BooleanObj(left.value || right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '||' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluateInfixAnd(left: IObject, right: IObject): IObject {
+    if (left instanceof BooleanObj && right instanceof BooleanObj) {
+        return new BooleanObj(left.value && right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '&&' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluateInfixPlus(left: IObject, right: IObject): IObject {
+    if (left instanceof IntObj && right instanceof IntObj) {
+        return new IntObj(left.value + right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '+' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluateInfixMinus(left: IObject, right: IObject): IObject {
+    if (left instanceof IntObj && right instanceof IntObj) {
+        return new IntObj(left.value - right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '-' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluateInfixAsterisk(left: IObject, right: IObject): IObject {
+    if (left instanceof IntObj && right instanceof IntObj) {
+        return new IntObj(left.value * right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '*' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluateInfixSlash(left: IObject, right: IObject): IObject {
+    if (left instanceof IntObj && right instanceof IntObj) {
+        return new IntObj(left.value / right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '/' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluateInfixReminder(left: IObject, right: IObject): IObject {
+    if (left instanceof IntObj && right instanceof IntObj) {
+        return new IntObj(left.value % right.value);
+    } else {
+        return new ErrorObj(
+            `cannot apply '%' on '${left.getType()}' and '${right.getType()}'`
+        );
+    }
+}
+
+function evaluatePrefixExpression(operator: string, right: IObject): IObject {
     if (right instanceof BooleanObj) {
         if (operator === '!') {
             return right.value ? FALSE : TRUE;
         } else {
             return new ErrorObj(
-                `unknown operator '${operator}' in expression '${right.toString()}'`
+                `unknown operator '${operator}' in expression '${right.getType()}'`
             );
         }
     } else if (right instanceof IntObj) {
@@ -53,11 +159,11 @@ function evaluatePrefixExpression(right: IObject, operator: string): IObject {
             }
             default:
                 return new ErrorObj(
-                    `unkown operator '${operator}' in expression '${right.toString()}'`
+                    `unkown operator '${operator}' in expression '${right.getType()}'`
                 );
         }
     } else {
-        return new ErrorObj(`invalid expression '${right.toString()}'`);
+        return new ErrorObj(`invalid expression '${right.getType()}'`);
     }
 }
 
